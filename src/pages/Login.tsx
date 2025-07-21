090a3e9f1a4b801cca8d4abfb0fd742ec67d89be
Jeeva <jeevav7305@gmail.com>
2025-07-21 12:37:22 +0530

    Add Forgot Password Link to Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -105,6 +105,11 @@ const Login = () => {
                   Forgot your password?
                 </Link>
               </p>
+              <p className="text-sm text-slate-600 mt-2">
+                <Link to="/forgot-password" className="text-slate-900 hover:underline font-medium">
+                  Forgot your password?
+                </Link>
+              </p>
             </div>
           </CardContent>
         </Card>
